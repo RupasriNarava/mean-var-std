d80941ae94ca4400cf05e54b1ae97726111205b5
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-28 10:59:17 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# This file is for local testing only.
+from mean_var_std import calculate
+
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))  # Sample input from freeCodeCamp
+
+# To run the unit tests:
+import test_module
